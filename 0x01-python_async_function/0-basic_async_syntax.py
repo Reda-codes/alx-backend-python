96e59a60904acf63f2d10e2c43297026804e1765
Reda <muhammadredabh@gmail.com>
2023-04-03 12:34:31 +0000

    Fixed type annotation

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
+''' asynchronous coroutine '''
 import asyncio
 import random
-''' asynchronous coroutine '''
 
 
-async def wait_random(max_delay=10: int) -> float:
+async def wait_random(max_delay: int = 10) -> float:
     ''' wait_random function '''
     rdm: float = random.uniform(0, max_delay)
     await asyncio.sleep(rdm)
