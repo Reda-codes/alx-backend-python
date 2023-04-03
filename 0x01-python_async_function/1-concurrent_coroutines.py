22b13c0439e63c3ce509107318441d4f306c36f0
Reda <muhammadredabh@gmail.com>
2023-04-03 13:09:09 +0000

    fix

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 ''' async routine '''
+from typing import Callable, Awaitable
 
 
-wait_random: Callable[[int], Awaitable[float]] = __import__('0-basic_async_syntax').wait_random
+wait_random: Callable[[int], Awaitable[float]]
+wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int = 10) -> list:
