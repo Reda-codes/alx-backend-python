6deb69e5d89b3f12bed55a1b40b92495c6708e08
Reda <muhammadredabh@gmail.com>
2023-04-24 20:52:07 +0000

    7. para

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -50,3 +50,13 @@ class TestGithubOrgClient(unittest.TestCase):
             self.assertEqual(ret, ["testing"])
             mock_get.assert_called_once
             mock_pub.assert_called_once
+
+    @parameterized.expand([
+        ({"license": {"key": "my_license"}}, "my_license", True),
+        ({"license": {"key": "other_license"}}, "my_license", False),
+        ])
+    def test_has_license(self, repo, license_key, expected_return):
+        """ test_has_license method """
+        test = GithubOrgClient("testing")
+        ret = test.has_license(repo, license_key)
+        self.assertEqual(expected_return, ret)
